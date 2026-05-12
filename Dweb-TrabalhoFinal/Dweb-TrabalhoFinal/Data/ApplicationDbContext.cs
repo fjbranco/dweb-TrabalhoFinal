cdb9f51ff0bae7d075e77942a63a012e810d1909
Filipe Branco <estt6967@ipt.pt>
2026-05-12 16:20:16 +0100

    Criação de classes do modelo de dados

--- Dweb-TrabalhoFinal/Dweb-TrabalhoFinal/Data/ApplicationDbContext.cs
+++ Dweb-TrabalhoFinal/Dweb-TrabalhoFinal/Data/ApplicationDbContext.cs
@@ -1,9 +1,20 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using ModeloDados.Models;
 
 namespace Dweb_TrabalhoFinal.Data
 {
+    /// <summary>
+    /// Base de dados do projeto da aplicação
+    /// </summary>
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
     {
+        /* tabelas da base de dados */
+        //public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Sala> Salas { get; set; }
+        //public DbSet<Genero> Generos { get; set; }
+        public DbSet<Bilhete> Bilhetes { get; set; }
+        public DbSet<Filme> Filmes { get; set; }
+        //public DbSet<Sessao> Sessoes { get; set; }
     }
 }
---
+++ Dweb-TrabalhoFinal/Dweb-TrabalhoFinal/Models/Filme.cs
@@ -0,0 +1,58 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ModeloDados.Models {
+    public class Filme {
+        /// <summary>
+        /// Chave primária do Filme
+        /// </summary>
+        [Key]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Título do filme
+        /// </summary>
+        public String Titulo { get; set; }
+
+        /// <summary>
+        /// Descrição do filme
+        /// </summary>
+        public String Descricao { get; set; }
+
+        /// <summary>
+        /// Nome do ficheiro da imagem do filme
+        /// </summary>
+        [StringLength(40)]
+        public string Imagem { get; set; } = "";
+
+        /// <summary>
+        /// Ano do Filme
+        /// </summary>
+        public String Ano { get; set; }
+
+        /// <summary>
+        /// Duração do filme
+        /// </summary>
+        public String Duracao { get; set; }
+
+        /* ********************************************
+         * Relacionamentos
+         * ******************************************** */
+
+        /// <summary>
+        /// FK da Sessão para o filme
+        /// </summary>
+        public ICollection<Sessao> SessaoFilme { get; set; } = new List<Sessao>();
+
+
+        /* ********************************************
+         * Relacionamentos N-M
+         * ******************************************** */
+
+        /// <summary>
+        /// Lista de Géneros que o filme pode ter
+        /// </summary>
+        public ICollection<Genero> ListOfGenero { get; set; } = [];
+
+
+    }
+}
