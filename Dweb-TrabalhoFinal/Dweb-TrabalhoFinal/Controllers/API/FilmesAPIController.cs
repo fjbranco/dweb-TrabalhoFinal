801dc6ddf3a210133cd62fdf98adc6fa06cf62f5
Filipe Branco <estt6967@ipt.pt>
2026-06-25 09:18:27 +0100

    Adicionado o controller FilmesAPIController para a API

---
+++ Dweb-TrabalhoFinal/Dweb-TrabalhoFinal/Controllers/API/FilmesAPIController.cs
@@ -0,0 +1,103 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ModeloDados.Models;
+using Dweb_TrabalhoFinal.Data;
+
+[Route("api/[controller]")]
+[ApiController]
+public class FilmesAPIController : ControllerBase
+{
+
+    /// <summary>
+    /// Referência à base de dados
+    /// </summary>
+    private readonly ApplicationDbContext _context;
+    public FilmesAPIController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    // GET: api/Filme
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Filme>>> GetFilme()
+    {
+        return await _context.Filmes.ToListAsync();
+    }
+
+    // GET: api/Filme/5
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Filme>> GetFilme(int id)
+    {
+        var filme = await _context.Filmes.FindAsync(id);
+
+        if (filme == null)
+        {
+            return NotFound();
+        }
+
+        return filme;
+    }
+
+    // PUT: api/Filme/5
+    // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutFilme(int? id, Filme filme)
+    {
+        if (id != filme.Id)
+        {
+            return BadRequest();
+        }
+
+        _context.Entry(filme).State = EntityState.Modified;
+
+        try
+        {
+            await _context.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            if (!FilmeExists(id))
+            {
+                return NotFound();
+            }
+            else
+            {
+                throw;
+            }
+        }
+
+        return NoContent();
+    }
+
+    // POST: api/Filme
+    // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+    [HttpPost]
+    public async Task<ActionResult<Filme>> PostFilme(Filme filme)
+    {
+        _context.Filmes.Add(filme);
+        await _context.SaveChangesAsync();
+
+        return CreatedAtAction("GetFilme", new { id = filme.Id }, filme);
+    }
+
+    // DELETE: api/Filme/5
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteFilme(int? id)
+    {
+        var filme = await _context.Filmes.FindAsync(id);
+        if (filme == null)
+        {
+            return NotFound();
+        }
+
+        _context.Filmes.Remove(filme);
+        await _context.SaveChangesAsync();
+
+        return NoContent();
+    }
+
+    private bool FilmeExists(int? id)
+    {
+        return _context.Filmes.Any(e => e.Id == id);
+    }
+}
