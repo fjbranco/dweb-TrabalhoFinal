acb40dde4fd85de53c09bba2f1fe19592e200c28
Filipe Branco <estt6967@ipt.pt>
2026-06-25 09:39:24 +0100

    Adicionado a ViewModel para aos dados de Filmes

---
+++ Dweb-TrabalhoFinal/Dweb-TrabalhoFinal/Models/ViewModels/FilmesDTO.cs
@@ -0,0 +1,31 @@
+﻿namespace Dweb_TrabalhoFinal.Models.ViewModels {
+    /// <summary>
+    /// Lista de filmes
+    /// </summary>
+    public class FilmesDTO {
+        /// <summary>
+        /// ID do filme na base de dados
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Título do filme
+        /// </summary>
+        public string Titulo { get; set; } = "";
+
+        /// <summary>
+        /// Descrição do filme
+        /// </summary>
+        public string? Descricao { get; set; }
+
+        /// <summary>
+        /// Nome do ficheiro da imagem do filme
+        /// </summary>
+        public string Imagem { get; set; } = "";
+
+        /// <summary>
+        /// Ano do filme
+        /// </summary>
+        public string Ano { get; set; } = "";
+    }
+}
