9512c78502727a4d38d578d8b79cbc06ae83490f
Filipe Branco <estt6967@ipt.pt>
2026-07-16 15:58:21 +0100

    Adicionado o brunch e criado RazorPages para Salas

---
+++ Dweb-TrabalhoFinal/Dweb-TrabalhoFinal/Pages/BilhetePages/Edit.cshtml.cs
@@ -0,0 +1,71 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ModeloDados.Models;
+using Dweb_TrabalhoFinal.Data;
+
+namespace Dweb-TrabalhoFinal.Pages.BilhetePages;
+
+public class EditModel : PageModel
+{
+    private readonly ApplicationDbContext _context;
+
+    public EditModel(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    [BindProperty]
+    public Bilhete Bilhete { get; set; } = default!;
+
+    public async Task<IActionResult> OnGetAsync(int? id)
+    {
+        if (id is null)
+        {
+            return NotFound();
+        }
+
+        var bilhete = await _context.Bilhetes.FirstOrDefaultAsync(m => m.Id == id);
+        if (bilhete is null)
+        {
+            return NotFound();
+        }
+        Bilhete = bilhete;
+        return Page();
+    }
+
+    // To protect from overposting attacks, enable the specific properties you want to bind to.
+    // For more details, see https://aka.ms/RazorPagesCRUD.
+    public async Task<IActionResult> OnPostAsync()
+    {
+        if (!ModelState.IsValid)
+        {
+            return Page();
+        }
+
+        _context.Attach(Bilhete).State = EntityState.Modified;
+
+        try
+        {
+            await _context.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            if (!BilheteExists(Bilhete.Id))
+            {
+                return NotFound();
+            }
+            else
+            {
+                throw;
+            }
+        }
+
+        return RedirectToPage("./Index");
+    }
+
+    private bool BilheteExists(int id)
+    {
+        return _context.Bilhetes.Any(e => e.Id == id);
+    }
+}
---
+++ Dweb-TrabalhoFinal/Dweb-TrabalhoFinal/Pages/BilhetePages/Index.cshtml.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ModeloDados.Models;
+using Dweb_TrabalhoFinal.Data;
+
+namespace Dweb-TrabalhoFinal.Pages.BilhetePages;
+
+public class IndexModel : PageModel
+{
+    private readonly ApplicationDbContext _context;
+
+    public IndexModel(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public IList<Bilhete> Bilhete { get; set; } = default!;
+
+    public async Task OnGetAsync()
+    {
+        Bilhete = await _context.Bilhetes.ToListAsync();
+    }
+}
