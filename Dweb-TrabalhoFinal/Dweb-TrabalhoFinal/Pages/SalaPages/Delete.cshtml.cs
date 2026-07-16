f3d43bc3ff2354b60042fe593eacb1b5c2a4a90d
Filipe Branco <estt6967@ipt.pt>
2026-07-16 15:24:47 +0100

    Adicionado o brunch e criado RazorPages para Salas

---
+++ Dweb-TrabalhoFinal/Dweb-TrabalhoFinal/Pages/SalaPages/Delete.cshtml.cs
@@ -0,0 +1,58 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ModeloDados.Models;
+using Dweb_TrabalhoFinal.Data;
+
+namespace Dweb-TrabalhoFinal.Pages.SalaPages;
+
+public class DeleteModel : PageModel
+{
+    private readonly ApplicationDbContext _context;
+
+    public DeleteModel(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    [BindProperty]
+    public Sala Sala { get; set; } = default!;
+
+    public async Task<IActionResult> OnGetAsync(int? id)
+    {
+        if (id is null)
+        {
+            return NotFound();
+        }
+
+        var sala = await _context.Salas.FirstOrDefaultAsync(m => m.Id == id);
+        if (sala is null)
+        {
+            return NotFound();
+        }
+        else
+        {
+            Sala = sala;
+        }
+
+        return Page();
+    }
+
+    public async Task<IActionResult> OnPostAsync(int? id)
+    {
+        if (id is null)
+        {
+            return NotFound();
+        }
+
+        var sala = await _context.Salas.FindAsync(id);
+        if (sala != null)
+        {
+            Sala = sala;
+            _context.Salas.Remove(Sala);
+            await _context.SaveChangesAsync();
+        }
+
+        return RedirectToPage("./Index");
+    }
+}
