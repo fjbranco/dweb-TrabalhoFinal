3abc3da9e0256383829abe92cd2b171e7f2c8f5a
dylan20 <malomardylankian@gmail.com>
2026-07-18 15:29:18 +0100

    adicionar o botao de compra na sessao

---
+++ Dweb-TrabalhoFinal/Dweb-TrabalhoFinal/Pages/Bilhetes/Comprar.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Dweb_TrabalhoFinal.Pages.Bilhetes
+{
+    public class ComprarModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
